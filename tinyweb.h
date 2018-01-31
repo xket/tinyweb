24f3ca8ed146090a3f59b81906b6cf6a8fa1eea7
cilamoo <sgydd001@gmail.com>
2018-01-31 23:27:24 +0800

    Update tinyweb.h

--- tinyweb.h
+++ tinyweb.h
@@ -90,6 +90,7 @@ void Munmap(void *start, size_t length);
 /* Process control wrappers */
 pid_t Fork(void);
 void Execve(const char *filename, char *const argv[], char *const envp[]);
+extern char **environ; /* defined by libc */
 
 void request_progress(int fd) 
 
