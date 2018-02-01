b0e3ce356b3e9c712a767bc49d0c4a890a9b8351
cilamoo <sgydd001@gmail.com>
2018-02-01 14:12:30 +0800

    Create wrappthread.c

---
+++ wrappthread.c
@@ -0,0 +1,31 @@
+/*
+ * pthreads wrapper functions.
+ */
+ 
+#include	"./tinyweb.h"
+#include	"./tinythread.h"
+
+void Pthread_create(pthread_t *tidp, pthread_attr_t *attrp, 
+		    void * (*routine)(void *), void *argp) 
+{
+    int rc;
+
+    if ((rc = pthread_create(tidp, attrp, routine, argp)) != 0)
+		  posix_error(rc, "Pthread_create error");
+}
+
+void Pthread_mutex_lock(pthread_mutex_t *mptr)  
+{  
+    int n = pthread_mutex_lock(mptr);  
+    if ( n == 0 )  
+        return;    
+    unix_error("pthread_mutex_lock error");  
+}  
+
+void Pthread_mutex_unlock(pthread_mutex_t *mptr)  
+{  
+    int n = pthread_mutex_unlock(mptr);  
+    if ( n == 0 )  
+        return;   
+    unix_error("pthread_mutex_unlock error");  
+}  
