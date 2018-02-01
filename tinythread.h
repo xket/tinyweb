d60e94e413cb7f939f81b7a47ae0c055c1726767
cilamoo <sgydd001@gmail.com>
2018-02-01 12:59:20 +0800

    Create tinythread.h

---
+++ tinythread.h
@@ -0,0 +1,13 @@
+* Our own header for the programs that use threads.
+   Include this file, instead of "tinyweb.h". */
+   
+#ifndef	__tinyweb_pthread_h
+#define	__tinyweb_pthread_h
+
+#include	"./tinyweb.h"
+
+void	Pthread_create(pthread_t *, const pthread_attr_t *,
+					   void * (*)(void *), void *);
+void	Pthread_join(pthread_t, void **);
+
+#endif	/* __tinyweb_pthread_h */
