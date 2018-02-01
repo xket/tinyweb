0dbeec16e38c4077869221ed73037e4e9bcf6901
cilamoo <sgydd001@gmail.com>
2018-02-01 11:27:16 +0800

    Update tinyweb.h

--- tinyweb.h
+++ tinyweb.h
@@ -93,6 +93,6 @@ pid_t Fork(void);
 void Execve(const char *filename, char *const argv[], char *const envp[]);
 extern char **environ; /* defined by libc */
 
-void request_progress(int fd) 
+void request_progress(int fd); 
 
 #endif /* __TinyWeb_H__ */  
