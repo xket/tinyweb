877163d40ede2e860619154248f2b7268d750db4
cilamoo <sgydd001@gmail.com>
2018-02-01 14:21:30 +0800

    Update tinyweb.c

--- tinyweb.c
+++ tinyweb.c
@@ -34,14 +34,6 @@ void *Malloc(size_t size)
     return ptr;    
 }
 	
-void Pthread_create(pthread_t *tidp, pthread_attr_t *attrp, 
-		    void * (*routine)(void *), void *argp) 
-{
-    int rc;
-
-    if ((rc = pthread_create(tidp, attrp, routine, argp)) != 0)
-		posix_error(rc, "Pthread_create error");
-}
 /*  
  * open_listenfd - open and return a listening socket on port
  *     Returns -1 and sets errno on Unix error.
@@ -132,22 +124,6 @@ Signal(int signo, Sigfunc *func)	/* for our signal() function */
 		posix_error(-1, "signal error");
 	return(sigfunc);
 }
-
-void Pthread_mutex_lock(pthread_mutex_t *mptr)  
-{  
-    int n = pthread_mutex_lock(mptr);  
-    if ( n == 0 )  
-        return;    
-    unix_error("pthread_mutex_lock error");  
-}  
-
-void Pthread_mutex_unlock(pthread_mutex_t *mptr)  
-{  
-    int n = pthread_mutex_unlock(mptr);  
-    if ( n == 0 )  
-        return;   
-    unix_error("pthread_mutex_unlock error");  
-}  
 
 /**********************************************************************
  * The Rio package - robust I/O functions                             *
