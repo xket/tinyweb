83353e932f90629230af4a64d4144e41b1775c81
cilamoo <sgydd001@gmail.com>
2018-02-01 14:12:05 +0800

    Update tinythread.h

--- tinythread.h
+++ tinythread.h
@@ -6,7 +6,7 @@
 
 #include	"./tinyweb.h"
 
-void	Pthread_create(pthread_t *, const pthread_attr_t *,
+void Pthread_create(pthread_t *, const pthread_attr_t *,
 					   void * (*)(void *), void *);
 void Pthread_mutex_lock(pthread_mutex_t *mptr);
 void Pthread_mutex_unlock(pthread_mutex_t *mptr);
